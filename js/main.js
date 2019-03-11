cb4dc5473750d49bbf8a4003da096906aaf2d5e3
Colby <colbytaylor1212@gmail.com>
2019-03-11 15:54:52 -0400

    Cleaned up js to function more quickly

--- js/main.js
+++ js/main.js
@@ -2,100 +2,33 @@ const url = 'https://entree-f18.herokuapp.com/v1/menu/8';
 //Set the main function to load all on load of page.
 //Each individual menu section will have a separate API call. 
 async function loadMenu(){
-   loadApps();
-   loadSands();
-   loadEntrees();
-   loadSides();
-   loadDesserts();
+   //loadApps()
+   loadMenuItems(8, "collap1");
+   //loadSand()
+   loadMenuItems(8, "collap2");
+   //loadEntrees()
+   loadMenuItems(8, "collap3");
+   //loadSides()
+   loadMenuItems(8, "collap4");
+   //loadDesserts()
+   loadMenuItems(8, "collap5");
 }
 
 //Api call for appetizers to load items and create the div cards. 
-async function loadApps() {
-   var app = "";
-
-   for (var i = 0; i < 8; i++) {
-       app = app + "<div class='card' id='item-'" + i + "><div class='card-body'>";
-       var output = await fetch(url).then(function (response) {
-           return response.json();
-       })
-           .then(function (myJson) {
-               console.log(JSON.stringify(myJson.menu_items[0].description));
-               app = app + JSON.stringify(myJson.menu_items[0].description) + "</div></div>";
-
-           });
-       console.log(output);
-       document.getElementById("collap1").innerHTML = app;
-   }
-}
-//Api call for sandwiches to load items and create the div cards.
-async function loadSands() {
-   var app = "";
-
-   for (var i = 0; i < 8; i++) {
-       app = app + "<div class='card' id='item-'" + i + "><div class='card-body'>";
-       var output = await fetch(url).then(function (response) {
-           return response.json();
-       })
-           .then(function (myJson) {
-               console.log(JSON.stringify(myJson.menu_items[0].description));
-               app = app + JSON.stringify(myJson.menu_items[0].description) + "</div></div>";
-
-           });
-       console.log(output);
-       document.getElementById('collap2').innerHTML = app;
-   }
-}
-//Api call for entrees to load items and create the div cards.
-async function loadEntrees() {
-   var app = "";
-
-   for (var i = 0; i < 8; i++) {
-       app = app + "<div class='card' id='item-'" + i + "><div class='card-body'>";
-       var output = await fetch(url).then(function (response) {
-           return response.json();
-       })
-           .then(function (myJson) {
-               console.log(JSON.stringify(myJson.menu_items[0].description));
-               app = app + JSON.stringify(myJson.menu_items[0].description) + "</div></div>";
-
-           });
-       console.log(output);
-       document.getElementById('collap3').innerHTML = app;
-   }
-}
-//Api call for sides to load items and create the div cards.
-async function loadSides() {
-   var app = "";
-
-   for (var i = 0; i < 8; i++) {
-       app = app + "<div class='card' id='item-'" + i + "><div class='card-body'>";
-       var output = await fetch(url).then(function (response) {
-           return response.json();
-       })
-           .then(function (myJson) {
-               console.log(JSON.stringify(myJson.menu_items[0].description));
-               app = app + JSON.stringify(myJson.menu_items[0].description) + "</div></div>";
-
-           });
-       console.log(output);
-       document.getElementById('collap4').innerHTML = app;
-   }
-}
-//Api call for desserts to load items and create the div cards.
-async function loadDesserts() {
+async function loadMenuItems(num, whereTo) {
     var app = "";
- 
-    for (var i = 0; i < 8; i++) {
-        app = app + "<div class='card' id='item-'" + i + "><div class='card-body'>";
-        var output = await fetch(url).then(function (response) {
+    var uri = url + num;
+        var output = await fetch(uri).then(function (response) {
             return response.json();
         })
-            .then(function (myJson) {
-                console.log(JSON.stringify(myJson.menu_items[0].description));
-                app = app + JSON.stringify(myJson.menu_items[0].description) + "</div></div>";
+        .then(function (myJson) {
  
-            });
-        console.log(output);
-        document.getElementById('collap5').innerHTML = app;
-    }
+            for (var i = 0; i < myJson.menu_items.length; i++) {
+                app += "<div class='card bg-dark text-white' id='item-" + i + "'><div class='card-body'>";
+                app += myJson.menu_items[i].description + "</div></div>";
+            }
+            console.log(output);
+            document.getElementById(whereTo).innerHTML = app;
+        });
+ 
  }
